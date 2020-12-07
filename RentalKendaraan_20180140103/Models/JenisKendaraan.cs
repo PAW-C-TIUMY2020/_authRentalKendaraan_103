21815c8025b8b0b0e6a9c828b95fc4c8266a1227
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-12-07 15:59:11 +0700

    Add project files.

---
+++ RentalKendaraan_20180140103/Models/JenisKendaraan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20180140103.Models
+{
+    public partial class JenisKendaraan
+    {
+        public JenisKendaraan()
+        {
+            Kendaraan = new HashSet<Kendaraan>();
+        }
+
+        public int IdJenisKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Nama jenis kendaraan tidak boleh kosong")]
+        public string NamaJenisKendaraan { get; set; }
+
+        public ICollection<Kendaraan> Kendaraan { get; set; }
+    }
+}
---
+++ RentalKendaraan_20180140103/Models/Pengembalian.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20180140103.Models
+{
+    public partial class Pengembalian
+    {
+        public int IdPengembalian { get; set; }
+
+        [Required(ErrorMessage = "Tanggal Pengembalian wajib diisi")]
+        public DateTime? TglPengembalian { get; set; }
+        public int? IdPeminjaman { get; set; }
+        public int? IdKondisi { get; set; }
+        public int? Denda { get; set; }
+
+        public KondisiKendaraan IdKondisiNavigation { get; set; }
+        public Peminjaman IdPeminjamanNavigation { get; set; }
+    }
+}
